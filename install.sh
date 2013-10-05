cc0ae816d0cb0216a94bcde8be92fc7c61997dc7
Robert Clark <rbclark@mitre.org>
2013-10-04 20:36:35 -0400

    Updated install.sh to actually be indented using spaces and follow conventions.

--- install.sh
+++ install.sh
@@ -1,46 +1,46 @@
 #!/bin/bash 
 
 if [ "$(id -u)" != "0" ]; then
-   echo "Please run again with sudo privileges." 1>&2
-   exit 1
+  echo "Please run again with sudo privileges." 1>&2
+  exit 1
 fi
 
 usage() { echo "Usage: $0 [-b <branch>]" 1>&2; exit 1; }
 
 while getopts ":b:h:" o; do
-    case "${o}" in
-        b)
-            branch=${OPTARG}
-            ;;
-        *)
-			usage
-			;;
-    esac
+  case "${o}" in
+    b)
+      branch=${OPTARG}
+      ;;
+    *)
+      usage
+     ;;
+  esac
 done
 
 cd /tmp
 
 # First, check to see if the command chef solo exists, if not download and install chef.
 if ! command -v chef-solo >/dev/null 2>&1; then
-	echo "Chef not found, downloading and installing now..."
-	curl -L https://opscode.com/chef/install.sh > chef-install.sh
-	chmod +x chef-install.sh
-	./chef-install.sh
+  echo "Chef not found, downloading and installing now..."
+  curl -L https://opscode.com/chef/install.sh > chef-install.sh
+  chmod +x chef-install.sh
+  ./chef-install.sh
 fi
 
 mkdir /tmp/cookbooks
 cd /tmp/cookbooks
 if command -v git >/dev/null 2>&1; then
-	echo "Git is installed, nothing to do here!"
+  echo "Git is installed, nothing to do here!"
 elif command -v apt-get >/dev/null 2>&1; then
-	echo "Installing git via apt..."
-	apt-get install -y git
+  echo "Installing git via apt..."
+  apt-get install -y git
 elif command -v yum >/dev/null 2>&1; then
-	echo "Installing git via yum..."
-	yum install -y git
+  echo "Installing git via yum..."
+  yum install -y git
 else
-	echo "Git is not installed and we do not know how to install it for your platform. Please install git and run this program again." 1>&2
-	exit 1
+  echo "Git is not installed and we do not know how to install it for your platform. Please install git and run this program again." 1>&2
+  exit 1
 fi
 
 git clone https://github.com/opscode-cookbooks/build-essential.git
@@ -62,8 +62,8 @@ cd ..
 tar -cvzf cookbooks.tar.gz cookbooks/
 
 if [ $branch ]; then
-	echo '{"popHealth":{"branch" : "'$branch'"}}' > node.json
-	chef-solo -r /tmp/cookbooks.tar.gz -o "apt,git,ruby-build,mongodb-10gen,popHealth" -j /tmp/node.json
+  echo '{"popHealth":{"branch" : "'$branch'"}}' > node.json
+  chef-solo -r /tmp/cookbooks.tar.gz -o "apt,git,ruby-build,mongodb-10gen,popHealth" -j /tmp/node.json
 else
-	chef-solo -r /tmp/cookbooks.tar.gz -o "apt,git,ruby-build,mongodb-10gen,popHealth"
+  chef-solo -r /tmp/cookbooks.tar.gz -o "apt,git,ruby-build,mongodb-10gen,popHealth"
 fi
