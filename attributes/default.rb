ef3211119f5f052176ba9a6212253c1a0ffe4e1c
Robert Clark <rbclark@mitre.org>
2014-05-02 11:17:25 -0400

    Bumped ruby version.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 default[:popHealth][:user] = "popHealth"
 default[:popHealth][:branch] = "master"
-default[:popHealth][:ruby_version] = "2.0.0-p353"
+default[:popHealth][:ruby_version] = "2.1.1"
 default[:popHealth][:passenger_version] = "4.0.35"
 default[:popHealth][:git_repository] = "https://github.com/pophealth/popHealth.git"
 default[:popHealth][:servername] = "localhost"
