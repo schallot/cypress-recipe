bfb6206ed6c4971f687750f50abe64d4780c7464
Robert Clark <rbclark@mitre.org>
2016-07-14 11:55:04 -0400

    Add git revision as an attribute that can be set for cypress.

--- attributes/default.rb
+++ attributes/default.rb
@@ -13,3 +13,4 @@ default[:cypress][:cypress_internal_port] = 8000
 default[:cypress][:cvu_install_path] = '/opt/cypress-validation-utility'
 default[:cypress][:cvu_internal_port] = 8001
 default[:cypress][:cvu_external_port] = 8080
+default[:cypress][:cypress_git_revision] = 'master'
--- recipes/install_cypress.rb
+++ recipes/install_cypress.rb
@@ -1,5 +1,6 @@
 cypress_install_app 'cypress' do
   application_path node[:cypress][:cypress_install_path]
   env_vars node[:cypress][:cypress_env_vars]
+  git_revision node[:cypress][:cypress_git_revision]
   unicorn_port node[:cypress][:cypress_internal_port]
 end
