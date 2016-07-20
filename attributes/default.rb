1f983b9535aa3e741b7fae22e6a6879eb8c73e39
Robert Clark <rbclark@mitre.org>
2016-07-20 16:47:30 -0400

    Add ability to set cypress validation utility git tag/branch.

--- attributes/default.rb
+++ attributes/default.rb
@@ -14,3 +14,4 @@ default[:cypress][:cvu_install_path] = '/opt/cypress-validation-utility'
 default[:cypress][:cvu_internal_port] = 8001
 default[:cypress][:cvu_external_port] = 8080
 default[:cypress][:cypress_git_revision] = 'master'
+default[:cypress][:cvu_git_revision] = 'master'
--- recipes/install_cvu.rb
+++ recipes/install_cvu.rb
@@ -1,6 +1,8 @@
 cypress_install_app 'cypress-validation-utility' do
   application_path node[:cypress][:cvu_install_path]
   git_repository 'https://github.com/projectcypress/cypress-validation-utility.git'
+  git_revision node[:cypress][:cvu_git_revision]
   delayed_job false
   unicorn_port node[:cypress][:cvu_internal_port]
+  generate_secrets_on_restart node[:cypress][:generate_secrets_on_restart]
 end
