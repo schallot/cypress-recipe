eb6fc9ccb778bf64686fa6cda70bd46b2549f81c
Robert Clark <rbclark@mitre.org>
2016-08-23 12:46:41 -0400

    Pin version numbers.

--- metadata.rb
+++ metadata.rb
@@ -5,9 +5,9 @@ license          'All rights reserved'
 description      'Installs/Configures Cypress'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
-depends          "application_ruby"
-depends          "apt"
-depends          "git"
-depends          'nginx_passenger'
-depends          "sudo"
-depends          "poise-ruby-build"
+depends          "application_ruby", "~> 4.0"
+depends          "apt",              "~> 4.0"
+depends          "git",              "~> 4.6"
+depends          'nginx_passenger',  "~> 0.5.7"
+depends          "sudo",             "~> 2.11"
+depends          "poise-ruby-build", "~> 1.0"
