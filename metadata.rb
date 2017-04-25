8d09f9e0451be2d9452018ebdf7f6449b4a93ec4
Clark Jr <rbclark@mitre.org>
2017-04-25 17:42:41 -0400

    Bump versions of dependencies and lock chef version at 12.19 for now since 13.0 breaks a lot of stuff

--- metadata.rb
+++ metadata.rb
@@ -6,8 +6,7 @@ description      'Installs/Configures Cypress'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
 depends          "application_ruby", "~> 4.0"
-depends          "apt",              "~> 4.0"
-depends          "git",              "~> 4.6"
-depends          'nginx_passenger',  "~> 0.5.7"
-depends          "sudo",             "~> 2.11"
+depends          "apt",              "~> 6.1"
+depends          "git",              "~> 6.0"
+depends          "sudo",             "~> 3.3"
 depends          "poise-ruby-build", "~> 1.0"
